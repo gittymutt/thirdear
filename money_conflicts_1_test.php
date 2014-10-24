1f5aab660ef62bd6824f23826ce9e0aaa774dfea
gittymutt <gittymutt@users.noreply.github.com>
2014-10-23 23:42:10 -0400

    make money_conflicts_1.php to test whether php will work. changed money_conflicts_1.html to money_conflicts_1.php. Add showit.php to text submission of  variable

---
+++ money_conflicts_1_test.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Third Ear Programme App</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+     <div class="container">
+  	    <div class="panel panel-success">
+        <div class="panel-heading panel-success">Money Conflicts: Step 1 - Define the Conflict.</div>
+       	<div class="panel-body">
+        I don't have enough money to...
+        <br><br>
+
+            <form action="showit.php" method="post">
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="pay my basic living expenses">  
+                     Pay my basic living expenses.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="create an emergency savings account"> 
+                     Create an emergency savings account.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="fix my car or things around my home"> 
+                     Create an emergency savings account.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="buy health insurance"> 
+                     Buy health insurance.</label>
+                <br>
+                    
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="save for retirement"> 
+                     Save for retirement.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="send my kids to college"> 
+                     Send my kids to college.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="take a vacation or have some fun"> 
+                     Take a vacation or have some fun.</label>
+                <br>
+
+                <label><input type="checkbox" name="notEnoughMoneyTo[]" value="(other)"> 
+                     (other)</label>
+                <br>
+                <br>
+                <input type="submit" class="btn btn-success">
+            </form>
+           </div>
+        </div>
+        </div>
+     </div>
+  </body>
+</html>
